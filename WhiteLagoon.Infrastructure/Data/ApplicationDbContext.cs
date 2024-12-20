8ddd96845faa4fef6379f94d7bf85db5546f5bac
Tinno <tinno@scanjet.net>
2024-12-20 14:53:02 +0700

    create villa table

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Infrastructure.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Villa> Villas {  get; set; }
     }
 }
